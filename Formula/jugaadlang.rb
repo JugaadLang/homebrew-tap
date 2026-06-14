7952ca4eb51ab485ba1f2637e893b2316cb62ac3
Sumangal Karan <123644743+Sumangal44@users.noreply.github.com>
2026-06-14 04:56:37 +0000

    jugaadlang: add 1.1.5 bottle.

--- Formula/jugaadlang.rb
+++ Formula/jugaadlang.rb
@@ -5,6 +5,13 @@ class Jugaadlang < Formula
   sha256 "935dcaa0cd0edfe270ac6cceb3db80e5ba6f4725c6dc9f898e045a45cb5cd25c"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/JugaadLang/homebrew-tap/releases/download/jugaadlang-1.1.5"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "6b45f4b1e31c49adcb967cf12aa0f30774fe4b63b2262ab7cc4fb1b41e8a9e07"
+    sha256 cellar: :any_skip_relocation, sequoia:      "0a29b834d769d4f9079649ecc116b431b8d8d6c9c8202a3f0a3cb1553ea86ae9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "47fb1c250a6727ccf95c75a189c5cb5fa24cafa09838c0d4e52feea33e96fc81"
+  end
+
   depends_on "python@3.12"
 
   def install
