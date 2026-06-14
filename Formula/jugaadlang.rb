6d915249fe2a312139f2837b9c24d2d53116b733
sumangal44 <sumangalkaran44@gmail.com>
2026-06-14 09:25:30 +0530

    Revert "Merge pull request #1 from JugaadLang/update-jugaadlang-v1.1.5"
    This reverts commit 97b8576c8e50e990ba1413f6cbb47acb9119df9f, reversing
    changes made to 79001af35287863887130862ba690f2b93860f71.

--- Formula/jugaadlang.rb
+++ Formula/jugaadlang.rb
@@ -1,8 +1,8 @@
 class Jugaadlang < Formula
   desc "Hindi-keyword programming language for Indian developers"
   homepage "https://jugaadlang.netlify.app"
-  url "https://github.com/jugaadlang/jugaadlang/archive/refs/tags/v1.1.5.tar.gz"
-  sha256 "935dcaa0cd0edfe270ac6cceb3db80e5ba6f4725c6dc9f898e045a45cb5cd25c"
+  url "https://github.com/jugaadlang/jugaadlang/archive/refs/tags/v1.1.4.tar.gz"
+  sha256 "0eeb6c482c46cd684b60e100880d8c5b5229bb8d8c1a901e3c32d0c94446d7e0"
   license "MIT"
 
   depends_on "python@3.12"
